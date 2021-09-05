f2e221a932803d4f8505d9998e4b049df5006a31
unknown <jdoucetoc@gmail.com>
2021-09-05 13:09:16 +0200

    Added Multiple views and Modified Models

--- sales/urls.py
+++ sales/urls.py
@@ -3,10 +3,11 @@ from rest_framework_nested import routers
 from . import views
 
 client_router = routers.SimpleRouter()
-client_router.register(r'clients', views.ClientsViewSet)
+client_router.register(r'clients', views.ClientsViewSet,
+                       basename='clients')
 
 contract_router = routers.NestedSimpleRouter(client_router, r'clients',
-                                             lookup='client')
+                                             lookup='clients')
 contract_router.register(r'contracts', views.ContractsViewSet,
                          basename='contracts')
 
